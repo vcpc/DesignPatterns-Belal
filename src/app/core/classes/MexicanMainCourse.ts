fae0b8d6a227f05711b5755d46bae7166cdf5476
marwanabubakr <dev@marobakr>
2024-05-15 13:37:27 +0300

    feat(@core/classes): create abstractFactory

--- src/app/core/classes/MexicanMainCourse.ts
+++ src/app/core/classes/MexicanMainCourse.ts
@@ -1,7 +1,7 @@
 import { MainCourse } from '../interfaces/mainCourse';
 
 /**
- * Class representing a mexican main course.
+ * Class representing a mexican main course
  */
 export class MexicanMainCourse implements MainCourse {
   name = 'mexicanMainCourse';
