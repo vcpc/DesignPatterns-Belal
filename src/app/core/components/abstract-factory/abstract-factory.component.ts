bc8e3b821d35631173af45918035645090af6f2e
haji <muhamedhajajii@outlook.com>
2024-05-15 13:03:27 +0300

    feat(@core): add abstract factory implementation

--- src/app/core/components/abstract-factory/abstract-factory.component.ts
+++ src/app/core/components/abstract-factory/abstract-factory.component.ts
@@ -3,25 +3,46 @@ import { Restaurants } from '../../interfaces/restaurant';
 import { ItalianRestaurantFactory } from '../../classes/italian-restaurant-factory';
 import { MexicanRestaurantFactory } from '../../classes/mexican-restaurant-factory';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-abstract-factory',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './abstract-factory.component.html',
   styleUrl: './abstract-factory.component.css',
 })
 export class AbstractFactoryComponent {
+  /**
+   * @property {string} clintCuisine to detect the clint choosen restaurant
+   */
+  clintCuisine: string = '';
+  /**
+   * @property {Restaurants} cuisines to choose class to choose cusines
+   */
   cuisines!: Restaurants;
+  /**
+   * @property {string} Appetizer one of the cuisines dishes value set depend on choosen cuisine
+   */
   Appetizer!: string;
+  /**
+   * @property {string} MainCourse  one of the cuisines dishes value set depend on choosen cuisine
+   */
   MainCourse!: string;
+  /**
+   * @property {string} Dessert one of the cuisines dishes value set depend on choosen cuisine
+   */
   Dessert!: string;
 
-  onCuisinesChange(e: any): void {
-    let cuisines = e.target.value;
-    if (cuisines === 'Italian') {
+  /**
+   * Handle Choosen Cuisines from select element
+   * the cusines build the menu depend on use choosen from Italian and Mexican
+   * making the making the choices between only two options making the possibility for the client to choose any unknown restaurants zero
+   */
+  onCuisinesChange(): void {
+    if (this.clintCuisine === 'Italian') {
       this.cuisines = new ItalianRestaurantFactory();
-    } else if (cuisines === 'Mexican') {
+    } else if (this.clintCuisine === 'Mexican') {
       this.cuisines = new MexicanRestaurantFactory();
     }
     this.Appetizer = this.cuisines.createAppetizer().name;
