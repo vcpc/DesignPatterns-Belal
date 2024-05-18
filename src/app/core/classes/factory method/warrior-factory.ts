f77136f00b1430cad8990dcb61e459db969aa817
marwanabubakr <dev@marobakr>
2024-05-18 15:20:10 +0300

    factoryMethod(@core): add factory method

--- src/app/core/classes/factory method/warrior-factory.ts
+++ src/app/core/classes/factory method/warrior-factory.ts
@@ -1,10 +1,16 @@
+import { Injectable } from '@angular/core';
 import { Character } from '../../interfaces/Factory Method/character';
 import { CharacterFactory } from './character-factory';
 import { Warrior } from './warrior';
+
+@Injectable({
+  providedIn: 'root',
+})
+
 /**
  * Class representing a Warrior Factory that creates Warrior characters.
  */
-class WarriorFactory extends CharacterFactory {
+export class WarriorFactory extends CharacterFactory {
   createCharacter(): Character {
     return new Warrior();
   }
